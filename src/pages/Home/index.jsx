92221fdcdf660f8db3c8785d67d7061424db4627
EstebanMa12 <desteban.maya@udea.edu.co>
2024-02-26 18:36:26 -0500

    Add dependencies and update file paths

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../../components/Navbar';
+const Home = () => {
+    return (
+        <div>
+        <Navbar />
+        <h1 className="text-3xl font-bold underline">Home</h1>
+        <p>This is the home page.</p>
+        </div>
+    );
+    }
+    export default Home;
