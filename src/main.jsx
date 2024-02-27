97c237ef50ee1b9da5cd625841434771541529fa
EstebanMa12 <desteban.maya@udea.edu.co>
2024-02-26 22:12:36 -0500

    Refactor main.jsx and AppRouter.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter'
 import './styles/index.css'
-// import { ProSidebarProvider} from 'react-pro-sidebar';
+import { BrowserRouter } from 'react-router-dom'
+import { ProSidebarProvider} from 'react-pro-sidebar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <AppRouter />
+      <ProSidebarProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </ProSidebarProvider>
   </React.StrictMode>,
 )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import {  Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import { useState } from "react";
+import SidebarComponent from "../components/SidebarComponent";
 
 
 
 function AppRouter() {
+  const [isSideBar, setIsSidebar] = useState(true);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+
+      <div className="app">
+        <SidebarComponent isSideBar={isSideBar} />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
   );
 }
 
