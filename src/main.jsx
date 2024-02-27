68e9863ee398c5eb7b5351165380dc8670582008
EstebanMa12 <desteban.maya@udea.edu.co>
2024-02-26 19:49:33 -0500

    Commented out ProSidebarProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter'
 import './styles/index.css'
-import { ProSidebarProvider} from 'react-pro-sidebar';
+// import { ProSidebarProvider} from 'react-pro-sidebar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProSidebarProvider>
       <AppRouter />
-    </ProSidebarProvider>
   </React.StrictMode>,
 )
