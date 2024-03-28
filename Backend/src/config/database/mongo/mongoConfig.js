33a461d45d329e2699f97c0ff834fd9b446e863a
EstebanMa12 <desteban.maya@udea.edu.co>
2024-03-27 23:12:55 -0500

    Update MongoDB configuration

--- Backend/src/config/database/mongo/mongoConfig.js
+++ Backend/src/config/database/mongo/mongoConfig.js
@@ -1,2 +1,26 @@
 import mongoose from 'mongoose';
 
+const configDB = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}
+
+export const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URI, configDB);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('Error connecting to MongoDB: ', error);
+    }
+}
+
+export const disconnect = async () => {
+    try {
+        await mongoose.disconnect();
+        console.log('MongoDB disconnected');
+    } catch (error) {
+        console.error('Error disconnecting from MongoDB: ', error);
+    }
+}
