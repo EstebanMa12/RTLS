067c4b5bfff01d327456afee99b373b03a7e547c
EstebanMa12 <desteban.maya@udea.edu.co>
2024-04-03 20:50:24 -0500

    Refactor user routes to use controller functions

--- Backend/src/config/routes/user-routes.js
+++ Backend/src/config/routes/user-routes.js
@@ -3,17 +3,17 @@ const { Router } = require("express");
 const UserRoutes = Router();
 
 const {
-    createUser,
-    getUser,
-    getUserInfo,
-    updateUser,
-    deleteUser,
+    createUserController,
+    getUserController,
+    getUserInfoController,
+    updateUserController,
+    deleteUserController,
 } = require("../../controllers/user-controller");
 
-UserRoutes.get('/user/', getUser);
-UserRoutes.post('/user/create', createUser);
-UserRoutes.get('/user/:id', getUserInfo);
-UserRoutes.patch('/user/update/:id', updateUser);
-UserRoutes.delete('/user/delete/:id', deleteUser);
+UserRoutes.get('/user/', getUserController);
+UserRoutes.post('/user/create', createUserController);
+UserRoutes.get('/user/:id', getUserInfoController);
+UserRoutes.patch('/user/update/:id', updateUserController);
+UserRoutes.delete('/user/delete/:id', deleteUserController);
 
 module.exports =  UserRoutes;
