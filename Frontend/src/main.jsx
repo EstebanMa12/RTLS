b05d4a9466ac3bde0e8d949cd745fa4a2402d09c
EstebanMa12 <desteban.maya@udea.edu.co>
2024-05-27 20:30:47 -0500

    chore: Update server.js, package.json, and vite.config.js

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,13 +5,16 @@ import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ProSidebarProvider} from 'react-pro-sidebar';
 import AppRouter from './router/index.jsx'
+import { ThemeProvider } from './components/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider>
       <ProSidebarProvider>
         <BrowserRouter>
           <AppRouter />
         </BrowserRouter>
       </ProSidebarProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -1,94 +1,103 @@
 /* eslint-disable no-unused-vars */
-import {Sidebar, Menu, MenuItem, SubMenu, useProSidebar} from 'react-pro-sidebar';
-import {  Route, Routes } from "react-router-dom";
-import {Link} from "react-router-dom";
+import {
+  Sidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  useProSidebar,
+} from "react-pro-sidebar";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
 import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import DevicesFoldIcon from '@mui/icons-material/DevicesFold';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import RegisterDevice from '../pages/RegisterDevice';
-import Report from '../pages/Report';
-import Calendar from '../pages/Calendar';
-import Devices from '../pages/Devices';
+import DevicesFoldIcon from "@mui/icons-material/DevicesFold";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import RegisterDevice from "../pages/RegisterDevice";
+import Report from "../pages/Report";
+import Calendar from "../pages/Calendar";
+import Devices from "../pages/Devices";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const AppRouter = () => {
-    const { collapseSidebar } = useProSidebar();
-    return (
-        <div className='flex h-full '>
-            <Sidebar style={{ height: "100vh"}}>
-            <Menu iconShape="square"
-
-            >
-                <MenuItem
-                    icon={<MenuOutlinedIcon/>}
-                    onClick={() => {
-                        collapseSidebar();
-                    }}
-                    style = {{cursor: "pointer", textAlign: "center"}}
-                >
-                    {" "}
-                    <h2 className="
+  const { collapseSidebar } = useProSidebar();
+  return (
+    <div className="flex h-full ">
+      <Sidebar style={{ height: "100vh" }}>
+        <Menu iconShape="square">
+          <MenuItem
+            icon={<MenuOutlinedIcon />}
+            onClick={() => {
+              collapseSidebar();
+            }}
+            style={{ cursor: "pointer", textAlign: "center" }}
+          >
+            {" "}
+            <h2
+              className="
                     text-2xl
                     font-bold
                     text-center
                     text-orange-800
-                    ">RTLS System</h2>
-                </MenuItem>
-                <MenuItem 
-                    icon={<HomeOutlinedIcon/>}
-                    component={<Link to="/"/>}
-                >
-                    Home
-                </MenuItem>
-                <MenuItem 
-                    icon={<PeopleOutlinedIcon />}
-                    component={<Link to="/about" />}
-                >
-                    About
-                </MenuItem>
-                <MenuItem
-                    icon={<DevicesFoldIcon />}
-                    component={<Link to="/devices" />}
-                >
-                    Devices Information
-                </MenuItem>
-                <MenuItem 
-                    icon={<ContactsOutlinedIcon />}
-                    component={<Link to="/register" />}
-                >
-                    Registrar Equipo
-                </MenuItem>
-                <MenuItem 
-                    icon={<ReceiptOutlinedIcon />}
-                    component={<Link to="/report" />}
-                >
-                    Reporte
-                </MenuItem>
-                <MenuItem 
-                    icon={<CalendarTodayOutlinedIcon />}
-                    component={<Link to="/calendar" />}
-                >
-                    Calendario
-                </MenuItem>
-            </Menu>
-            </Sidebar>
-            <section className=' w-full'>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/register" element={<RegisterDevice />} />
-                    <Route path="/report" element={<Report />} />
-                    <Route path="/calendar" element={<Calendar />} />
-                    <Route path="/devices" element={<Devices/>} /> 
-                    <Route path="*" element={<h1>Not Found</h1>} />
-                </Routes>
-            </section>
-        </div>
-    );
-    }
-    export default AppRouter;
+                    "
+            >
+              RTLS System
+            </h2>
+          </MenuItem>
+          <MenuItem icon={<HomeOutlinedIcon />} component={<Link to="/" />}>
+            Home
+          </MenuItem>
+          <MenuItem
+            icon={<PeopleOutlinedIcon />}
+            component={<Link to="/about" />}
+          >
+            About
+          </MenuItem>
+          <MenuItem
+            icon={<DevicesFoldIcon />}
+            component={<Link to="/devices" />}
+          >
+            Devices Information
+          </MenuItem>
+          <MenuItem
+            icon={<ContactsOutlinedIcon />}
+            component={<Link to="/register" />}
+          >
+            Registrar Equipo
+          </MenuItem>
+          <MenuItem
+            icon={<ReceiptOutlinedIcon />}
+            component={<Link to="/report" />}
+          >
+            Reporte
+          </MenuItem>
+          <MenuItem
+            icon={<CalendarTodayOutlinedIcon />}
+            component={<Link to="/calendar" />}
+          >
+            Calendario
+          </MenuItem>
+          <div className="flex justify-end mt-8">
+            <ModeToggle />
+          </div>
+        </Menu>
+      </Sidebar>
+      <section className=" w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<RegisterDevice />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/devices" element={<Devices />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </section>
+    </div>
+  );
+};
+export default AppRouter;
