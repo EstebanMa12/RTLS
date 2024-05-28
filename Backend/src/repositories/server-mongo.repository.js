c4e5dbbb669ff4c6d552c9350dd431aad95af768
EstebanMa12 <desteban.maya@udea.edu.co>
2024-05-27 23:50:57 -0500

    Update createSensorValue function to include state parameter

--- Backend/src/repositories/server-mongo.repository.js
+++ Backend/src/repositories/server-mongo.repository.js
@@ -1,147 +1,178 @@
 
 const { SensorDB, ButtonDB } = require("../models/values.model");
 
-
-const getValues = async () => {
-    try {
-        return ValuesDB.find();
-    }catch (error){
-        console.log(error)
-    }
-}
-
-const getValuesByDate = async (date) => {
-    try {
-        if (!date) {
-            return ValuesDB.find();
-        }
-        
-        const startOfDay = `${date}T00:00:00.000Z`;
-        const endOfDay = `${date}T23:59:59.999Z`;
-
-        return ValuesDB.find({
-            date: {
-                $gte: startOfDay,
-                $lte: endOfDay
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const getValuesByHour = async (hour) => {
-    try {
-        if (!hour) {
-            return ValuesDB.find();
-        }
-        const date = new Date(); // Obtiene la fecha actual
-        date.setHours(hour - 5); // Establece la hora especificada
-        date.setMinutes(0); // Establece los minutos a 0
-        date.setSeconds(0); // Establece los segundos a 0
-        date.setMilliseconds(0); // Establece los milisegundos a 0
-
-        const startOfHour = new Date(date.getTime()); // Inicio de la hora
-        const endOfHour = new Date(date.getTime() + 3599999); // Fin de la hora (3599999 milisegundos = 59 minutos y 59.999 segundos)
-        console.log(date)
-        return ValuesDB.find({
-            date: {
-                $gte: startOfHour,
-                $lte: endOfHour
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const getValuesByHourRange = async (startHour, endHour) => {
-    try {
-        if (!startHour || !endHour) {
-            return ValuesDB.find();
-        }
-        const date = new Date(); // Obtiene la fecha actual
-        date.setHours(startHour-5); // Establece la hora especificada
-        date.setMinutes(0); // Establece los minutos a 0
-        date.setSeconds(0); // Establece los segundos a 0
-        date.setMilliseconds(0); // Establece los milisegundos a 0
-
-        const startOfHour = new Date(date.getTime()); // Inicio de la hora
-
-        date.setHours(endHour-5); // Establece la hora especificada
-        date.setMinutes(59); // Establece los minutos a 59
-        date.setSeconds(59); // Establece los segundos a 59
-        date.setMilliseconds(999); // Establece los milisegundos a 999
-
-        const endOfHour = new Date(date.getTime()); // Fin de la hora
-
-
-
-        return ValuesDB.find({
-            date: {
-                $gte: startOfHour,
-                $lte: endOfHour
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const getValuesByDateRange = async (startDate, endDate) => {
-    try {
-        if (!startDate || !endDate) {
-            return ValuesDB.find();
-        }
-        
-        const startOfDay = `${startDate}T00:00:00.000Z`;
-        const endOfDay = `${endDate}T23:59:59.999Z`;
-
-        return ValuesDB.find({
-            date: {
-                $gte: startOfDay,
-                $lte: endOfDay
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 const createRecord = async (data) => {
     try {
         const { movementState, buttonState } = data;
         const movementEntry = new SensorDB({ movement: movementState });
-        const stateEntry = new ButtonDB({ state:buttonState });
+        const stateEntry = new ButtonDB({ state: buttonState });
 
         await movementEntry.save();
         await stateEntry.save();
         return { movementEntry, stateEntry };
-    }catch (error){
+    } catch (error) {
         console.error('Error: ', error);
         throw error;
     }
 }
 
+const calculateUsageStatistics = async (startDate, endDate) => {
+    try {
+        if (!startDate || !endDate) {
+            return SensorDB.find();
+        }
 
+        const startOfDay = `${startDate}T00:00:00.000Z`;
+        const endOfDay = `${endDate}T23:59:59.999Z`;
+
+        const values = SensorDB.find({
+            date: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        });
+        let activeTime = 0;
+        let inactiveTime = 0;
+        values.forEach((entry, index) => {
+            if (entry.movement) {
+                activeTime += 20;
+            } else {
+                inactiveTime += 20;
+            }
+        })
+
+        return { activeTime: activeTime / 60, inactiveTime: inactiveTime / 60 };
+
+
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+
+const generateUsageReport = async (startDate, endDate) => {
+    try {
+        if (!startDate || !endDate) {
+            return SensorDB.find();
+        }
+        const usageStats = calculateUsageStatistics(startDate, endDate);
+
+        return {
+            starDate,
+            endDate,
+            activeTime: usageStats.activeTime,
+            inactiveTime: usageStats.inactiveTime,
+            usagePercentage: (usageStats.activeTime / (usageStats.activeTime + usageStats.inactiveTime)) * 100
+        }
+
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+
+const checkForAlerts = async () => {
+    try {
+        const now = new Date();
+        const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+        const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+        const usageStats = calculateUsageStatistics(startOfDay, endOfDay);
+
+        if (usageStats.inactiveTime > 60 * 4) {
+            return { alert: true, message: 'No se ha detectado movimiento en las últimas 4 horas' };
+        }
+        return { alert: false, message: 'No hay alertas' };
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+const getValuesByDate = async (date) => {
+    try {
+        if (!date) {
+            return SensorDB.find();
+        }
+
+        const startOfDay = `${date}T00:00:00.000Z`;
+        const endOfDay = `${date}T23:59:59.999Z`;
+
+        const values = await SensorDB.find({
+            date: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        });
+
+        const buttons = await ButtonDB.find({
+            date: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        });
+
+        return { values, buttons };
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+const getLastValues = async () => {
+    try {
+        const values = await SensorDB.find().sort({ date: -1 }).limit(1);
+        const buttons = await ButtonDB.find().sort({ date: -1 }).limit(1);
+
+        return { values, buttons };
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+
+// SENSOR CRUD OPERATIONS
 // Update
 const updateValue = async (id, value) => {
     try {
-        return ValuesDB.findByIdAndUpdate
-        (id, value, {new: true});
+        return SensorDB.findByIdAndUpdate
+            (id, value, { new: true });
     }
-    catch (error){
-        console.log(error)
+    catch (error) {
+        console.error('Error: ', error);
+        throw error;
     }
 }
 
 // Delete
 const deleteValue = async (id) => {
     try {
-        return ValuesDB.findByIdAndDelete(id);
+        return SensorDB.findByIdAndDelete(id);
     }
-    catch (error){
-        console.log(error)
+    catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+
+// BUTTON CRUD OPERATIONS
+// Update
+const updateButton = async (id, value) => {
+    try {
+        return ButtonDB.findByIdAndUpdate
+            (id, value, { new: true });
+    }
+    catch (error) {
+        console.error('Error: ', error);
+        throw error;
+    }
+}
+
+// Delete
+const deleteButton = async (id) => {
+    try {
+        return ButtonDB.findByIdAndDelete(id);
+    }
+    catch (error) {
+        console.error('Error: ', error);
+        throw error;
     }
 }
 
@@ -151,9 +182,12 @@ module.exports = {
     updateValue,
     deleteValue,
     getValuesByDate,
-    getValuesByHour,
-    getValuesByHourRange,
-    getValuesByDateRange
+    calculateUsageStatistics,
+    generateUsageReport,
+    checkForAlerts,
+    getLastValues,
+    updateButton,
+    deleteButton
 }
 
 
